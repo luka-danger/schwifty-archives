dcd673d06769bd4138b86cb9c591ca0af20c804a
luka-danger <lukathevegan@gmail.com>
2025-01-16 09:47:07 -0700

    Add comments

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import { searchCharacters } from "./search.js";
 import { goHome } from "./goHome.js";
 import { loadPage } from "./loadPage.js";
 
+// Current URL
 let currentPage = 'https://rickandmortyapi.com/api/character';
 
+// Make buttons active when DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#searchBtn').addEventListener('click', searchCharacters)
     document.querySelector('#homeBtn').addEventListener('click', goHome)
