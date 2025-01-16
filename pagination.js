1956c160358ba362f75c405f1f7130fcf2480dab
luka-danger <lukathevegan@gmail.com>
2025-01-16 11:10:24 -0700

    Clear console when clicking next and previous buttons

--- pagination.js
+++ pagination.js
@@ -7,14 +7,20 @@ export function pagination(info) {
     if (info.prev) {
         const prevBtn = document.createElement('button')
         prevBtn.textContent = 'Previous'
-        prevBtn.addEventListener('click', () => loadPage(info.prev))
+        prevBtn.addEventListener('click', () => {
+            loadPage(info.prev)
+            console.clear()
+        })
         pagination.appendChild(prevBtn)
     }
 
     if (info.next) {
         const nextBtn = document.createElement('button');
         nextBtn.textContent = 'Next';
-        nextBtn.addEventListener('click', () => loadPage(info.next))
+        nextBtn.addEventListener('click', () => {
+            loadPage(info.next), 
+            console.clear()
+        })
         pagination.appendChild(nextBtn)
     }
 }
