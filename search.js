e2d36488ec032e299a33c1e998dac8802e381df9
luka-danger <lukathevegan@gmail.com>
2025-01-16 11:07:51 -0700

    Clear console when new character is searched

--- search.js
+++ search.js
@@ -3,6 +3,7 @@ import { loadPage } from "./loadPage.js"
 let url = 'https://rickandmortyapi.com/api/character'
 
 export async function searchCharacters() {
+    console.clear()
     const query = document.querySelector('#searchInput').value.trim()
     if (query) {
         const searchUrl = `${url}/?name=${query}`
